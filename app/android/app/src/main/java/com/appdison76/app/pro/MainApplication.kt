2042e2512f347b0eca3f22cbf4973b6b1335e0a9
appdison76 <appdison76@gmail.com>
2026-03-01 20:02:09 +0900

    알림플레이어원복패키지도pro모두맞춤

--- app/android/app/src/main/java/com/appdison76/app/pro/MainApplication.kt
+++ app/android/app/src/main/java/com/appdison76/app/pro/MainApplication.kt
@@ -5,6 +5,7 @@ import android.content.res.Configuration
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.appdison76.app.pro.MediaSessionPackage
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
@@ -23,8 +24,7 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(MediaSessionPackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
